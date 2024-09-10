0e69820c48bc03736bd981502342d01b0ed9dde7
Jishad <jishadjishu200260@gmail.com>
2024-09-10 14:09:12 +0530

    bsd

--- include/slider.php
+++ include/slider.php
@@ -9,6 +9,7 @@
                             <h1 class="m-b-20"><strong>B-VOC<br>Department of Farook College</strong></h1>
                             <!-- <p class="m-b-40">See how your users experience your website in realtime or view <br> trends to see any changes in performance over time.</p> -->
                          
+                            
                         </div>
                     </div>
                 </div>
